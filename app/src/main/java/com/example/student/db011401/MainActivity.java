1d0a912ba32d14ad1bd1a284c67bc2a52c6e2bcb
shaojung <shaojung@gmail.com>
2017-01-14 10:26:55 +0800

    ratingBar

--- app/src/main/java/com/example/student/db011401/MainActivity.java
+++ app/src/main/java/com/example/student/db011401/MainActivity.java
@@ -4,13 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.RatingBar;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     ProgressBar pb, pb2;
     SeekBar sb;
-    TextView tv2;
+    TextView tv2, tv3, tv4, tv5;
+    RatingBar rb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
         pb2 = (ProgressBar) findViewById(R.id.progressBar2);
         sb = (SeekBar) findViewById(R.id.seekBar);
         tv2 = (TextView) findViewById(R.id.textView2);
+        tv3 = (TextView) findViewById(R.id.textView3);
+        tv4 = (TextView) findViewById(R.id.textView4);
+        tv5 = (TextView) findViewById(R.id.textView5);
+        rb = (RatingBar) findViewById(R.id.ratingBar);
 
         sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -50,5 +56,11 @@ public class MainActivity extends AppCompatActivity {
     public void clickAdd(View v)
     {
         pb2.setProgress(pb2.getProgress() + 10);
+    }
+    public void clickRead(View v)
+    {
+        tv3.setText(String.valueOf(sb.getProgress()));
+        tv4.setText(String.valueOf(rb.getProgress()));
+        tv5.setText(String.valueOf(rb.getRating()));
     }
 }
